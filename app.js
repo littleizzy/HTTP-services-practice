da5256e7674abb0eee140cd73da7f4e77574d55b
Izzy Zhao <izzy_zxy@hotmail.com>
2018-10-15 22:02:00 -0700

    added DELETE /api/courses/:id

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var courses = [
 
 app.get('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) res.status(404).send("Given course id not found, fuck off please.");
+	if (!course) return res.status(404).send("Given course id not found, fuck off please.");
 	res.send(course);
 });
 
@@ -27,10 +27,7 @@ app.get('/api/courses/:id', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
 	var { error } = validateCourse(req.body); //object destructure
-	if (error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 
 	const course = {
 		id : courses.length + 1,
@@ -43,17 +40,22 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) res.status(404).send("Given course id not found, fuck off please.");
+	if (!course) return res.status(404).send("Given course id not found, fuck off please.");
 
 	var { error } = validateCourse(req.body); //object destructure
-	if (error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 
 	course.name = req.body.name;
 	res.send(course);
-})
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	if (!course) return res.status(404).send("Given course id not found, fuck off please.");
+
+	var index = courses.indexOf(course);
+	courses.splice(index, 1);	
+});
 
 
 function validateCourse(course) {
